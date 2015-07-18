49d539f85a17794420a5cad6ef15fb2de60e002d
kohey <k.kouhei18@gmail.com>
2015-07-18 18:15:06 +0900

    Initial Commit

---
+++ LiveStreamingSampleTests/LiveStreamingSampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  LiveStreamingSampleTests.swift
+//  LiveStreamingSampleTests
+//
+//  Created by kohey on 2015/07/18.
+//  Copyright (c) 2015年 kohey. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class LiveStreamingSampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
